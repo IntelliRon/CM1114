10750a226ab2e84687156d89b6e9e635fcf08fec
IntelliRon <gulpsabook@gmail.com>
2023-03-23 22:01:53 +0000

    Fixed some html, tidied code and added shop page

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,15 +1,19 @@
+// Function to view an image in the full image viewer
+// REQUIRES: A div with id "imgViewer" and an img with id "imgViewerImg"
 function viewImage(url) {
     document.getElementById("imgViewer").style = "display: block;";
     document.getElementById("imgViewerImg").src = url;
 }
 
+// Function to hide the image viewer
+// REQUIRES: A div with id "imgViewer"
 function hideFullImage() {
     document.getElementById("imgViewer").style = "display: none;";
 }
 
+// Function that adds grayscale and blur filters to all images except hovered image
 function hoverImage(event) {
     if (document.getElementById("imgViewer").style.display == "none") {
-        console.log("Hello");
         let element = event.target;
         element.style = "border: solid blue 2px; filter: none;";
         for (e of document.getElementsByClassName("galleryImg")) {
@@ -20,6 +24,7 @@ function hoverImage(event) {
     }
 }
 
+// Function that removes filters from all images
 function removeHoverImage(event) {
     let element = event.target;
     element.style = "border: none; filter: none;";
@@ -30,6 +35,7 @@ function removeHoverImage(event) {
     }
 }
 
+// Loop through all images to give them the necessary eventListeners to execute above functions
 for (e of document.getElementsByClassName("galleryImg")) {
     e.addEventListener("mouseover", hoverImage);
     e.addEventListener("focus", hoverImage);
